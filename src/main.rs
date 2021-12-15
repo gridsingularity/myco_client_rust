d0f43ea99d628616120f67aed1edda5b2811a3ac
Lana <svetlana.konstantinovna@gmail.com>
2021-12-15 15:26:28 +0100

    DD-101: deserealize responses
    Subscribe to two channels and match response

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,24 @@
 extern crate redis;
 mod redis_subscriber;
+mod pay_as_bid;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let channel = String::from("external-myco/*/recommendations/");
+    let channel1 = String::from("external-myco/*/offers-bids/response/");
+    let channel2 = String::from("external-myco/*/recommendations");
 
-    if let Err(error) = redis_subscriber::psubscribe(channel.clone()) {
+    if let Err(error) = redis_subscriber::psubscribe(channel1.clone()) {
         println!("{:?}", error);
         panic!("{:?}", error);
     } else {
-        println!("subscribed to channel: {}", channel);
+        println!("subscribed to channel: {}", channel1);
+    }
+
+    if let Err(error) = redis_subscriber::psubscribe(channel2.clone()) {
+        println!("{:?}", error);
+        panic!("{:?}", error);
+    } else {
+        println!("subscribed to channel: {}", channel2);
     }
 
     Ok(())
--- src/pay_as_bid.rs
+++ src/pay_as_bid.rs
@@ -1,26 +1,43 @@
 use serde::{Serialize, Deserialize};
+use chrono::{DateTime, Utc};
 
 const FLOATING_POINT_TOLERANCE: f32 = 0.00001;
 
 #[derive(Clone, Debug, PartialEq, PartialOrd, Serialize, Deserialize)]
 pub struct Bid {
-    buyer: String,
-    energy_rate: f32,
+    r#type: String,
+    id: String,
     energy: f32,
-    id: u8,
+    energy_rate: f32,
+    original_price: f32,
+    time_slot: DateTime<Utc>,
+    attributes: Option<String>,
+    requirements: Option<String>,
+    buyer_origin: String,
+    buyer_origin_id: String,
+    buyer_id: String,
+    buyer: String,
 }
 
 #[derive(Clone, Debug, PartialEq, PartialOrd, Serialize, Deserialize)]
 pub struct Offer {
-    seller: String,
-    energy_rate: f32,
+    r#type: String,
+    id: String,
     energy: f32,
-    id: u8,
+    energy_rate: f32,
+    original_price: f32,
+    time_slot: DateTime<Utc>,
+    attributes: Option<String>,
+    requirements: Option<String>,
+    seller_origin: String,
+    seller_origin_id: String,
+    seller_id: String,
+    seller: String,
 }
 
 #[derive(Clone, Debug, PartialEq, PartialOrd, Serialize, Deserialize)]
 pub struct BidOfferMatch {
-    market_id: u8,
+    market_id: String,
     bids: Vec<Bid>,
     selected_energy: f32,
     offers: Vec<Offer>,
@@ -29,17 +46,17 @@ pub struct BidOfferMatch {
 
 #[derive(Clone, Debug, PartialEq, PartialOrd, Serialize, Deserialize)]
 pub struct MatchingData {
-    bids: Vec<Bid>,
-    offers: Vec<Offer>,
+    pub bids: Vec<Bid>,
+    pub offers: Vec<Offer>,
 }
 
 pub trait GetMatchesRecommendations {
-    fn get_matches_recommations(&self, mut matching_data: MatchingData) -> Vec<BidOfferMatch> {
+    fn get_matches_recommendations(&self, mut matching_data: MatchingData) -> Vec<BidOfferMatch> {
         let mut bid_offer_pairs = Vec::new();
-        
+
         matching_data.bids.sort_by(|a, b| b.energy_rate.partial_cmp(&a.energy_rate).unwrap());
         matching_data.offers.sort_by(|a, b| b.energy_rate.partial_cmp(&a.energy_rate).unwrap());
-        
+
         let mut already_selected_bids = Vec::new();
         for offer in matching_data.offers.clone() {
             for bid in matching_data.bids.clone() {
@@ -47,10 +64,10 @@ pub trait GetMatchesRecommendations {
                     continue;
                 }
                 if offer.energy_rate - bid.energy_rate <= FLOATING_POINT_TOLERANCE {
-                    already_selected_bids.push(bid.id);
+                    already_selected_bids.push(bid.id.clone());
                     let selected_energy = bid.energy.min(offer.energy);
                     let new_bid_offer_match = BidOfferMatch {
-                            market_id: bid.id,
+                            market_id: bid.id.clone(),
                             bids: vec![bid.clone()],
                             selected_energy: selected_energy,
                             offers: vec![offer.clone()],

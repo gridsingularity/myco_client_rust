7e3bca1d8f8c85088e2bb5be935e8eef7249caf7
Lana <svetlana.konstantinovna@gmail.com>
2021-12-16 11:47:02 +0100

    DD-101: rename channels

--- src/main.rs
+++ src/main.rs
@@ -4,21 +4,21 @@ mod pay_as_bid;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let channel1 = String::from("external-myco/*/offers-bids/response/");
-    let channel2 = String::from("external-myco/*/recommendations");
+    let orders_response_channel = String::from("external-myco/*/offers-bids/response/");
+    let recommendations_channel = String::from("external-myco/*/recommendations");
 
-    if let Err(error) = redis_subscriber::psubscribe(channel1.clone()) {
+    if let Err(error) = redis_subscriber::psubscribe(orders_response_channel.clone()) {
         println!("{:?}", error);
         panic!("{:?}", error);
     } else {
-        println!("subscribed to channel: {}", channel1);
+        println!("subscribed to channel: {}", orders_response_channel);
     }
 
-    if let Err(error) = redis_subscriber::psubscribe(channel2.clone()) {
+    if let Err(error) = redis_subscriber::psubscribe(recommendations_channel.clone()) {
         println!("{:?}", error);
         panic!("{:?}", error);
     } else {
-        println!("subscribed to channel: {}", channel2);
+        println!("subscribed to channel: {}", recommendations_channel);
     }
 
     Ok(())

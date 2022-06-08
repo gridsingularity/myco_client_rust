dc7d1ff2ec8d17b800a9ab6d18e52f625f4a1c02
Felice Rocchitelli <felice@gridsingularity.com>
2022-06-08 17:07:15 +0200

    [DD-165] feature: Added the balance transfer extrinsic to test the integration.

--- src/connectors/substrate_connector.rs
+++ src/connectors/substrate_connector.rs
@@ -1,12 +1,13 @@
 use crate::primitives::web3::{Bid, Offer, Order, OrderSchema, OrderStatus};
 use anyhow::{Error, Result};
 use async_recursion::async_recursion;
+use sp_keyring::AccountKeyring;
 use std::sync::{Arc, Mutex};
 use std::{thread, time};
 use subxt::{
     rpc::Subscription,
     sp_runtime::{generic::Header, traits::BlakeTwo256},
-    ClientBuilder, DefaultConfig, SubstrateExtrinsicParams,
+    ClientBuilder, DefaultConfig, PairSigner, PolkadotExtrinsicParams, SubstrateExtrinsicParams,
 };
 use text_colorizer::*;
 
@@ -32,10 +33,11 @@ pub async fn substrate_subscribe(orderbook_url: String, node_url: String) -> Res
     while let Some(Ok(block)) = gsy_blocks_events.next().await {
         eprintln!("Block {:?} finalized: {:?}", block.number, block.hash());
 
-        if (block.number as u64) % 1 == 0 {
+        if (block.number as u64) % 4 == 0 {
             eprintln!("{}", "Starting matching cycle".green());
 
             let orderbook_url_clone = Arc::clone(&orderbook_url);
+            let node_url_clone = Arc::clone(&node_url);
 
             if let Err(error) = tokio::task::spawn(async move {
                 let orderbook_url_clone = orderbook_url_clone.lock().unwrap().to_string();
@@ -59,12 +61,20 @@ pub async fn substrate_subscribe(orderbook_url: String, node_url: String) -> Res
                 let open_bid: Vec<Bid> = body
                     .clone()
                     .into_iter()
-                    .filter(|order| order.status == OrderStatus::Open && matches!(order.order, Order::Bid{..}))
-                    .map(|order| order.into()).collect();
+                    .filter(|order| {
+                        order.status == OrderStatus::Open
+                            && matches!(order.order, Order::Bid { .. })
+                    })
+                    .map(|order| order.into())
+                    .collect();
                 let open_offer: Vec<Offer> = body
                     .into_iter()
-                    .filter(|order| order.status == OrderStatus::Open && matches!(order.order, Order::Offer{..}))
-                    .map(|order| order.into()).collect();
+                    .filter(|order| {
+                        order.status == OrderStatus::Open
+                            && matches!(order.order, Order::Offer { .. })
+                    })
+                    .map(|order| order.into())
+                    .collect();
                 eprintln!("{} - {:?}", "Open Bid".blue(), open_bid);
                 eprintln!("{} - {:?}", "Open Offer".magenta(), open_offer);
             })
@@ -77,7 +87,50 @@ pub async fn substrate_subscribe(orderbook_url: String, node_url: String) -> Res
                 );
             }
 
-            // TODO: Add Extrinsic creation and broadcast
+            // TODO: Modify Extrinsic to SettleTrade
+            tokio::task::spawn(async move {
+                let node_url_clone = node_url_clone.lock().unwrap().to_string();
+
+                let signer = PairSigner::new(AccountKeyring::Alice.pair());
+                eprintln!("Signer: {:?}", signer.account_id());
+                let dest = AccountKeyring::Bob.to_account_id().into();
+                let api =
+                    ClientBuilder::new()
+                        .set_url(node_url_clone)
+                        .build()
+                        .await
+                        .unwrap_or_else(|e| panic!("Failed to connect to node: {:?}", e))
+                        .to_runtime_api::<gsy_node::RuntimeApi<
+                            DefaultConfig,
+                            PolkadotExtrinsicParams<DefaultConfig>,
+                        >>();
+
+                let balance_transfer = api
+                    .tx()
+                    .balances()
+                    .transfer(dest, 10_000)
+                    .unwrap_or_else(|e| panic!("Failed to create the transfer extrinsic: {:?}", e))
+                    .sign_and_submit_then_watch_default(&signer)
+                    .await
+                    .unwrap_or_else(|e| panic!("Failed to submit the transfer extrinsic: {:?}", e))
+                    .wait_for_finalized_success()
+                    .await
+                    .unwrap_or_else(|e| {
+                        panic!(
+                        "Failed to fetch a successful response for the transfer extrinsic: {:?}",
+                        e
+                    )
+                    });
+
+                let transfer_event = balance_transfer
+                    .find_first::<gsy_node::balances::events::Transfer>()
+                    .unwrap_or_else(|e| panic!(
+                        "Failed to ensure that the transaction was successful by catching the associated events. {:?}",
+                        e
+                    ));
+
+                eprintln!("Balance transfer success: {:?}", transfer_event);
+            });
         }
     }
     eprintln!("{}", "Subscription dropped.".bright_red().bold());
